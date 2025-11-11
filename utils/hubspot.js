fdef802f2da34dd8a6cf793a78ce831b62def647
SangareswariS <sangareswari@syngrid.com>
2025-11-11 15:32:15 +0530

    Create pets module added

---
+++ utils/hubspot.js
@@ -0,0 +1,50 @@
+const axios = require('axios');
+
+const HUBSPOT_ACCESS_TOKEN = process.env.HUBSPOT_ACCESS_TOKEN;
+const BASE_URL = 'https://api.hubapi.com';
+const HUBSPOT_CUSTOM_OBJECT_TYPE = 'p244342521_pets';
+
+const headers = {
+  Authorization: `Bearer ${HUBSPOT_ACCESS_TOKEN}`,
+  'Content-Type': 'application/json',
+};
+
+// Fetch all pets
+async function getAllPets() {
+  try {
+    const response = await axios.get(
+      `${BASE_URL}/crm/v3/objects/${HUBSPOT_CUSTOM_OBJECT_TYPE}`,
+      {
+        headers,
+        params: {
+          properties: 'pet_name,pet_type,pet_bio,lastmodifieddate',
+          limit: 10,
+          after: 0,
+          sorts: '-createdAt' // Sorts by createdate in descending order
+        },
+      }
+    );
+
+    const pets = response.data.results;
+
+    if (!pets || pets.length === 0) {
+      console.log('No records found in HubSpot.');
+      return [];
+    }
+
+    return pets.map((pet) => ({
+      id: pet.id,
+      name: pet.properties.pet_name || 'Unnamed',
+      type: pet.properties.pet_type || 'Unknown',
+      bio: pet.properties.pet_bio || 'No bio available',
+    }));
+  } catch (error) {
+    console.error('Error fetching pets:', error.response?.data || error.message);
+    return [];
+  }
+}
+
+
+module.exports = {
+  getAllPets  
+};
